b10458240ebe5a466ea9e3798e3a56e2be51c65d
gdalle <dalleguillaume@yahoo.com>
2017-11-09 23:27:36 +0100

    Essentiellement rien n'a change pour que tout change

--- proj2/extraction.py
+++ proj2/extraction.py
@@ -3,14 +3,14 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
-from time import time
 
 
 def memory(df):
     """Print memory usage."""
     mem = df.memory_usage() / (1024**2)
-    print("Memory usage (MB) :", mem.sum())
+    print("\nMemory usage (MB) :", mem.sum())
     print(mem)
+    print("")
 
 
 def to_day(date, start_year=2015):
@@ -22,54 +22,51 @@ def to_day(date, start_year=2015):
 
 def read_train():
     """Read train."""
-    train1 = pd.read_csv("data/train.csv")
+    print("\nREADING TRAIN\n")
     train2 = pd.read_csv("data/train_v2.csv")
-    train2 = train2[~train2.msno.isin(train1.msno.unique())]
-    train = pd.merge(train1, train2, how="outer")
+    # train1 = pd.read_csv("data/train.csv")
+    # train2 = train2[~train2.msno.isin(train1.msno.unique())]
+    # train = pd.merge(train1, train2, how="outer")
     # train.index = train["msno"]
     # train = train.drop("msno", axis=1)
+    # del train1
+    # del train2
 
-    memory(train)
+    train = train2
+    # memory(train)
     train["is_churn"] = train["is_churn"].astype(np.int8)
     memory(train)
 
-    del train1
-    del train2
     return train
-
-
-train = read_train()
 
 
 # Test table
 
 def read_test():
     """Read test."""
+    print("\nREADING TEST\n")
     test2 = pd.read_csv("data/sample_submission_v2.csv")
     # test1 = pd.read_csv("data/sample_submission_zero.csv")
     # test2 = test2[~test2.msno.isin(test1.msno.unique())]
     # test = pd.merge(test1, test2, how="outer")
     # test.index = test["msno"]
     # test = test.drop("msno", axis=1)
-
-    test = test2
-    memory(test)
-    test = test.drop("is_churn", axis=1)
-    memory(test)
-
     # del test1
     # del test2
 
+    test = test2
+    # memory(test)
+    test = test.drop("is_churn", axis=1)
+    memory(test)
+
     return test
-
-
-test = read_test()
 
 
 # Members table
 
 def read_members():
     """Read members."""
+    print("\nREADING MEMBERS\n")
     dtype_cols_members = {
         'msno': object,
         'city': np.int64,
@@ -82,10 +79,10 @@ def read_members():
 
     members1 = pd.read_csv("data/members.csv", dtype=dtype_cols_members)
     members2 = pd.read_csv("data/members_v2.csv", dtype=dtype_cols_members)
-    members2 = members2[~members2.msno.isin(members1.msno.unique())]
+    # members2 = members2[~members2.msno.isin(members1.msno.unique())]
     members = pd.merge(members1, members2, how="outer")
 
-    memory(members)
+    # memory(members)
 
     # members.index = members["msno"]
     # members = members.drop("msno", axis=1)
@@ -113,22 +110,16 @@ def read_members():
     return members
 
 
-members = read_members()
-
-# Merging train / test with members as a way of getting first features
-
-train = pd.merge(train, members, how='left', on='msno')
-train.index = train["msno"]
-train = train.drop("msno", axis=1)
-test = pd.merge(test, members, how='left', on='msno')
-test.index = test["msno"]
-test = test.drop("msno", axis=1)
-
-
 # Transactions table
 
-def read_transactions():
+def read_transactions(train, test, max_lines=np.inf):
     """Read transactions."""
+    print("\nREADING TRANSACTIONS\n")
+    # Useful ids
+    id_train = set(train.index.unique())
+    id_test = set(test.index.unique())
+    useful_msno = set.union(id_train, id_test)
+
     dtype_cols_transactions = {
         'msno': object,
         'payment_method_id': np.int64,
@@ -141,15 +132,57 @@ def read_transactions():
         'is_cancel': np.int64
     }
 
-    transactions1 = pd.read_csv(
-        "data/transactions.csv", dtype=dtype_cols_transactions)
-    transactions2 = pd.read_csv(
-        "data/transactions_v2.csv", dtype=dtype_cols_transactions)
-    transactions2 = \
-        transactions2[~transactions2.msno.isin(transactions1.msno.unique())]
-    transactions = pd.merge(transactions1, transactions2, how="outer")
+    transactions_list1 = []
+    transactions_list2 = []
 
-    memory(transactions)
+    chunk_number = 0
+    for df in pd.read_csv(
+        "data/transactions.csv",
+        chunksize=10**5,
+        iterator=True,
+        header=0,
+        dtype=dtype_cols_transactions
+    ):
+        append_condition = df['msno'].isin(useful_msno)
+        df = df[append_condition]
+        transactions_list1.append(df)
+        print("Chunk {} of table 1 read".format(chunk_number))
+        chunk_number += 1
+        if chunk_number >= max_lines / (10**5):
+            break
+
+    transactions1 = pd.concat(transactions_list1, ignore_index=True)
+
+    chunk_number = 0
+    for df in pd.read_csv(
+        "data/transactions_v2.csv",
+        chunksize=10**5,
+        iterator=True,
+        header=0,
+        dtype=dtype_cols_transactions
+    ):
+        append_condition = df['msno'].isin(useful_msno)
+        df = df[append_condition]
+        transactions_list2.append(df)
+        print("Chunk {} of table 2 read".format(chunk_number))
+        chunk_number += 1
+        if chunk_number >= max_lines / (10**5):
+            break
+
+    transactions2 = pd.concat(transactions_list2, ignore_index=True)
+
+    # transactions1 = pd.read_csv(
+    #     "data/transactions.csv", dtype=dtype_cols_transactions)
+    # transactions2 = pd.read_csv(
+    #     "data/transactions_v2.csv", dtype=dtype_cols_transactions)
+    # transactions2 = \
+    #     transactions2[~transactions2.msno.isin(transactions1.msno.unique())]
+
+    transactions = pd.merge(
+        transactions1, transactions2,
+        how="outer", sort=False)
+
+    # memory(transactions)
     transactions.index = transactions.index.astype(np.int32)
     transactions['payment_method_id'] = \
         transactions['payment_method_id'].astype(np.int8)
@@ -183,13 +216,11 @@ def read_transactions():
     return transactions
 
 
-transactions = read_transactions()
-
-
 # User logs table
 
-def read_user_logs(train, test, max_lines=4*(10**7)):
+def read_user_logs(train, test, max_lines=np.inf):
     """Read user logs."""
+    print("\nREADING USER LOGS\n")
     # Useful ids
     id_train = set(train.index.unique())
     id_test = set(test.index.unique())
@@ -207,9 +238,10 @@ def read_user_logs(train, test, max_lines=4*(10**7)):
         'total_secs': np.float32
     }
 
-    user_logs = None
+    user_logs_list1 = []
+    user_logs_list2 = []
+
     chunk_number = 0
-    user_logs_list = []
     for df in pd.read_csv(
         "data/user_logs.csv",
         chunksize=10**5,
@@ -220,12 +252,36 @@ def read_user_logs(train, test, max_lines=4*(10**7)):
         append_condition = df['msno'].isin(useful_msno)
         df = df[append_condition]
         df["date"] = pd.to_datetime(df["date"])
-        user_logs_list.append(df)
-        print("Chunk {} read".format(chunk_number))
+        user_logs_list1.append(df)
+        print("Chunk {} of table 1 read".format(chunk_number))
         chunk_number += 1
         if chunk_number >= max_lines / (10**5):
             break
-    user_logs = pd.concat(user_logs_list, ignore_index=True)
+
+    user_logs1 = pd.concat(user_logs_list1, ignore_index=True)
+
+    chunk_number = 0
+    for df in pd.read_csv(
+        "data/user_logs_v2.csv",
+        chunksize=10**5,
+        iterator=True,
+        header=0,
+        dtype=dtype_cols_user_logs
+    ):
+        append_condition = df['msno'].isin(useful_msno)
+        df = df[append_condition]
+        df["date"] = pd.to_datetime(df["date"])
+        user_logs_list2.append(df)
+        print("Chunk {} of table 2 read".format(chunk_number))
+        chunk_number += 1
+        if chunk_number >= max_lines / (10**5):
+            break
+
+    user_logs2 = pd.concat(user_logs_list2, ignore_index=True)
+
+    user_logs = pd.merge(
+        user_logs1, user_logs2,
+        how="outer", sort=False, copy=False)
 
     user_logs['msno'] = user_logs['msno'].astype('category')
     for col in ['num_25', 'num_50', 'num_75', 'num_985', 'num_100', 'num_unq']:
@@ -236,15 +292,33 @@ def read_user_logs(train, test, max_lines=4*(10**7)):
     user_logs['day'] = t3.dt.day.astype(np.int8)
     user_logs = user_logs.drop("date", axis=1)
     memory(user_logs)
+
+    del user_logs1
+    del user_logs2
     return user_logs
 
 
-user_logs = read_user_logs(train, test)
+# MAIN
 
-user_logs.max()
+# User dataframes
 
+train = read_train()
+test = read_test()
+members = read_members()
 
-test["is_churn"] = np.random.rand(len(test))
+train.index = train["msno"]
+train = train.drop("msno", axis=1)
+test.index = test["msno"]
+test = test.drop("msno", axis=1)
+
+train["is_churn"].sum() / len(train)
+
+# Transactions and user logs dataframes
+
+transactions = read_transactions(train, test, max_lines=10**6)
+user_logs = read_user_logs(train, test, max_lines=10**6)
+
+test["is_churn"] = np.random.rand(len(test))*0.1
 submission = test.loc[:, ["is_churn"]]
 len(submission)
 submission.to_csv("data/submission.csv")
