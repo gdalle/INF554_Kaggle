1eed3ef8c2c2ab710fa70911e3c723f586ce86e7
gdalle <dalleguillaume@yahoo.com>
2017-11-10 10:39:27 +0100

    First feature extraction and prediction model

--- proj2/extraction.py
+++ proj2/extraction.py
@@ -3,19 +3,23 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+from datetime import datetime
 
 
 def memory(df):
-    """Print memory usage."""
+    """Print memory usage of a DataFrame."""
     mem = df.memory_usage() / (1024**2)
     print("\nMemory usage (MB) :", mem.sum())
     print(mem)
     print("")
 
 
-def to_day(date, start_year=2015):
-    """Count the number of days since 01/01/start_year."""
-    return date.dayofyear + 365 * (date.year - start_year)
+def categorize(df, col):
+    """Turn categorical variables into 0/1 columns for each possible value."""
+    cat = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df, cat], axis=1)
+    df = df.drop(col, axis=1)
+    return df
 
 
 # Train table
@@ -33,7 +37,8 @@ def read_train():
     # del train2
 
     train = train2
-    # memory(train)
+
+    # Change integer storage to gain memory
     train["is_churn"] = train["is_churn"].astype(np.int8)
     memory(train)
 
@@ -55,7 +60,8 @@ def read_test():
     # del test2
 
     test = test2
-    # memory(test)
+
+    # Lose the bogus target column
     test = test.drop("is_churn", axis=1)
     memory(test)
 
@@ -79,24 +85,24 @@ def read_members():
 
     members1 = pd.read_csv("data/members.csv", dtype=dtype_cols_members)
     members2 = pd.read_csv("data/members_v2.csv", dtype=dtype_cols_members)
-    # members2 = members2[~members2.msno.isin(members1.msno.unique())]
+    members2 = members2[~members2.msno.isin(members1.msno.unique())]
     members = pd.merge(members1, members2, how="outer")
 
-    # memory(members)
-
-    # members.index = members["msno"]
-    # members = members.drop("msno", axis=1)
-
+    # Recode genrer as integers
     members["gender"] = members["gender"].replace("male", 1)
     members["gender"] = members["gender"].replace("female", 2)
     members["gender"] = members["gender"].replace(np.NaN, 0)
     members["gender"] = members["gender"].astype(np.int8)
 
+    # Change integer storage
     members['city'] = members['city'].astype(np.int8)
     members['bd'] = members['bd'].astype(np.int16)
     members['registered_via'] = members['registered_via'].astype(np.int8)
 
+    # Drop expiration date, which isn't available for everyone
     members = members.drop("expiration_date", axis=1)
+
+    # Split date on three columns
     t = pd.to_datetime(members["registration_init_time"])
     members['registration_init_year'] = t.dt.year.astype(np.int16)
     members['registration_init_month'] = t.dt.month.astype(np.int8)
@@ -135,6 +141,7 @@ def read_transactions(train, test, max_lines=np.inf):
     transactions_list1 = []
     transactions_list2 = []
 
+    # Read data by chunks to alleviate memory load
     chunk_number = 0
     for df in pd.read_csv(
         "data/transactions.csv",
@@ -153,6 +160,7 @@ def read_transactions(train, test, max_lines=np.inf):
 
     transactions1 = pd.concat(transactions_list1, ignore_index=True)
 
+    # Read data by chunks to alleviate memory load
     chunk_number = 0
     for df in pd.read_csv(
         "data/transactions_v2.csv",
@@ -175,14 +183,15 @@ def read_transactions(train, test, max_lines=np.inf):
     #     "data/transactions.csv", dtype=dtype_cols_transactions)
     # transactions2 = pd.read_csv(
     #     "data/transactions_v2.csv", dtype=dtype_cols_transactions)
-    # transactions2 = \
-    #     transactions2[~transactions2.msno.isin(transactions1.msno.unique())]
+
+    transactions2 = \
+        transactions2[~transactions2.msno.isin(transactions1.msno.unique())]
 
     transactions = pd.merge(
         transactions1, transactions2,
         how="outer", sort=False)
 
-    # memory(transactions)
+    # Change integer storage
     transactions.index = transactions.index.astype(np.int32)
     transactions['payment_method_id'] = \
         transactions['payment_method_id'].astype(np.int8)
@@ -197,12 +206,14 @@ def read_transactions(train, test, max_lines=np.inf):
     transactions['is_cancel'] = \
         transactions['is_cancel'].astype(np.int8)
 
+    # Split date on three columns
     t1 = pd.to_datetime(transactions["transaction_date"])
     transactions['transaction_year'] = t1.dt.year.astype(np.int16)
     transactions['transaction_month'] = t1.dt.month.astype(np.int8)
     transactions['transaction_day'] = t1.dt.day.astype(np.int8)
     transactions = transactions.drop("transaction_date", axis=1)
 
+    # Split date on three columns
     t2 = pd.to_datetime(transactions["membership_expire_date"])
     transactions['membership_expire_year'] = t2.dt.year.astype(np.int16)
     transactions['membership_expire_month'] = t2.dt.month.astype(np.int8)
@@ -241,6 +252,7 @@ def read_user_logs(train, test, max_lines=np.inf):
     user_logs_list1 = []
     user_logs_list2 = []
 
+    # Read data by chunks to alleviate memory load
     chunk_number = 0
     for df in pd.read_csv(
         "data/user_logs.csv",
@@ -260,6 +272,7 @@ def read_user_logs(train, test, max_lines=np.inf):
 
     user_logs1 = pd.concat(user_logs_list1, ignore_index=True)
 
+    # Read data by chunks to alleviate memory load
     chunk_number = 0
     for df in pd.read_csv(
         "data/user_logs_v2.csv",
@@ -284,13 +297,18 @@ def read_user_logs(train, test, max_lines=np.inf):
         how="outer", sort=False, copy=False)
 
     user_logs['msno'] = user_logs['msno'].astype('category')
+
+    # Change integer storage
     for col in ['num_25', 'num_50', 'num_75', 'num_985', 'num_100', 'num_unq']:
         user_logs[col] = user_logs[col].astype(np.int8)
+
+    # Split date on three columns
     t3 = user_logs["date"]
     user_logs['year'] = t3.dt.year.astype(np.int16)
     user_logs['month'] = t3.dt.month.astype(np.int8)
     user_logs['day'] = t3.dt.day.astype(np.int8)
     user_logs = user_logs.drop("date", axis=1)
+
     memory(user_logs)
 
     del user_logs1
@@ -310,15 +328,67 @@ train.index = train["msno"]
 train = train.drop("msno", axis=1)
 test.index = test["msno"]
 test = test.drop("msno", axis=1)
-
-train["is_churn"].sum() / len(train)
+members.index = members["msno"]
+members = members.drop("msno", axis=1)
 
 # Transactions and user logs dataframes
 
 transactions = read_transactions(train, test, max_lines=10**6)
 user_logs = read_user_logs(train, test, max_lines=10**6)
 
-test["is_churn"] = np.random.rand(len(test))*0.1
+# Feature extraction
+
+print("\nFEATURE EXTRACTION\n")
+
+# Users on which we have info
+train_useful = train[
+    train.index.isin(members.index) &
+    train.index.isin(transactions["msno"]) &
+    train.index.isin(user_logs["msno"].astype(object))
+].copy()
+test_useful = test.loc[
+    test.index.isin(members.index) &
+    test.index.isin(transactions["msno"]) &
+    test.index.isin(user_logs["msno"].astype(object))
+].copy()
+
+
+def add_members_info(user_table, members):
+    """Extract info from members and add it to train or test."""
+    user_table["registration_init_year"] = \
+        members.loc[user_table.index, ["registration_init_year"]]
+    user_table["city"] = members.loc[user_table.index, ["city"]]
+    user_table = categorize(user_table, "city")
+    return user_table
+
+
+# Add features directly taken from members table
+train_useful = add_members_info(train_useful, members)
+test_useful = add_members_info(test_useful, members)
+
+# Prediction
+
+print("\nPREDICTION\n")
+
+# Conversion into arrays for scikit-learn
+x = np.array(train_useful.drop("is_churn", axis=1))
+y = np.array(train_useful["is_churn"])
+xt = np.array(test_useful)
+
+# Linear Regression
+clf = linear_model.LinearRegression()
+clf.fit(x, y)
+yt = clf.predict(xt)
+a = yt < 0
+b = yt > 1
+c = a | b
+yt[c] = 0
+
+# Random prediction as baseline
+test["is_churn"] = np.random.rand(len(test)) * 0.1
+# For users on which we have more info, use it
+test.loc[test_useful.index, ["is_churn"]] = yt.reshape(-1, 1)
+
+# Save as csv
 submission = test.loc[:, ["is_churn"]]
-len(submission)
 submission.to_csv("data/submission.csv")
